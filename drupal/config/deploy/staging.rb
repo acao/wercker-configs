0bd147cb5b539ea1e372a836b268f846ddc093c1
Richard Schulte <richard.j.schulte@gmail.com>
2014-07-07 10:13:11 -0400

    Change staging server name
    For the sake of consistency!

--- drupal/config/deploy/staging.rb
+++ drupal/config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server "prod.com", :app, :web
+server "stage.com", :app, :web
 set :user, "www-data"
 set :deploy_to, "/var/www/#{application}/staging"
 set :shared_root, "/var/www/#{application}/shared"
